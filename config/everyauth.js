fba29d1d067c8677913b552ee7a71a206bfc671a
Jhon Jimenez <jhonb3r@gmail.com>
2013-12-29 17:43:52 -0800

    Added support to authentication with password

--- config/everyauth.js
+++ config/everyauth.js
@@ -6,6 +6,9 @@ module.exports = function(everyauth, config){
   var nextUserId = 0;
   var usersByGoogleId = {};
   var usersByGhId = {};
+  var usersByLogin = {
+    'brian@example.com': addUser({ login: 'brian@example.com', password: 'password'})
+  };
 
   function addUser (source, sourceUser) {
     var user;
@@ -78,5 +81,67 @@ module.exports = function(everyauth, config){
         return usersByGhId[ghUser.id] || (usersByGhId[ghUser.id] = addUser('github', ghUser));
     })
     .redirectPath('/');
+
+  everyauth.password
+    .loginWith('email')
+    .getLoginPath('/login')
+    .postLoginPath('/login')
+    .loginView('login.jade')
+//    .loginLocals({
+//      title: 'Login'
+//    })
+//    .loginLocals(function (req, res) {
+//      return {
+//        title: 'Login'
+//      }
+//    })
+    .loginLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async login'
+        });
+      }, 200);
+    })
+    .authenticate( function (login, password) {
+      var errors = [];
+      if (!login) errors.push('Missing login');
+      if (!password) errors.push('Missing password');
+      if (errors.length) return errors;
+      var user = usersByLogin[login];
+      if (!user) return ['Login failed'];
+      if (user.password !== password) return ['Login failed'];
+      return user;
+    })
+
+    .getRegisterPath('/register')
+    .postRegisterPath('/register')
+    .registerView('register.jade')
+//    .registerLocals({
+//      title: 'Register'
+//    })
+//    .registerLocals(function (req, res) {
+//      return {
+//        title: 'Sync Register'
+//      }
+//    })
+    .registerLocals( function (req, res, done) {
+      setTimeout( function () {
+        done(null, {
+          title: 'Async Register'
+        });
+      }, 200);
+    })
+    .validateRegistration( function (newUserAttrs, errors) {
+      var login = newUserAttrs.login;
+      if (usersByLogin[login]) errors.push('Login already taken');
+      return errors;
+    })
+    .registerUser( function (newUserAttrs) {
+      var login = newUserAttrs[this.loginKey()];
+      return usersByLogin[login] = addUser(newUserAttrs);
+    })
+
+    .loginSuccessRedirect('/')
+    .registerSuccessRedirect('/');
 
 }
